af1c0d25786a650dcdd03cca38c7f09a57d8f639
Ajay Kumar <ajay@keepworks.com>
2023-10-12 15:53:38 +0530

    Fixed env vars names

--- src/Laravel/config.php
+++ src/Laravel/config.php
@@ -2,7 +2,10 @@
 
 return [
     'public_key' => env('DASHX_PUBLIC_KEY'),
+
     'private_key' => env('DASHX_PRIVATE_KEY'),
-    'target_environment' => env('DASHX_TARGET_ENVIROMENT'),
-    'base_uri' => env('DASHX_URI'),
+
+    'target_environment' => env('DASHX_TARGET_ENVIRONMENT'),
+
+    'base_uri' => env('DASHX_BASE_URI'),
 ];
